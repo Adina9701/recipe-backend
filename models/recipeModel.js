69184e7ee3366c10712ceb8ff3ddc3a57ca28b41
Adina9701 <adina.phbs@gmail.com>
2025-07-28 16:55:41 +0530

    update model

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,13 +1,13 @@
 const { default: mongoose } = require("mongoose");
 
 const recipeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    ingredients: { type: [String], required: true },
-    instructions: { type: String, required: true },
-  });
-  
-  
-  const Recipe = mongoose.model('Recipe', recipeSchema);
-  
-  module.exports = Recipe;
-  
+  name: { type: String, required: true },
+  ingredients: { type: [String], required: true },
+  instructions: { type: String, required: true },
+  serves: { type: Number, required: false },
+  prepTime: { type: Number, required: false },
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
